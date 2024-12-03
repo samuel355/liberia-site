341606d3e639e9b6ddeffda6d62d257ed2a80fcd
Samuel Osei Adu <asamuel355@yahoo.com>
2024-12-03 08:42:59 +0000

    update testimonials

--- about-us.php
+++ about-us.php
@@ -419,113 +419,11 @@
                             </div>
                         </div>
                     </div>
-                    <!-- section   -->
-                    <div class="content-section">
-                        <div class="container  ">
-                            <div class="section-title">
-                                <h4>What said about us</h4>
-                                <h2>Testimonials and Clients</h2>
-                            </div>
-                        </div>
-                        <div class="testimonilas-carousel-wrap">
-                            <div class="testimonilas-carousel">
-                                <div class="swiper-container">
-                                    <div class="swiper-wrapper">
-                                        <!--testi-item-->
-                                        <div class="swiper-slide">
-                                            <div class="testi-item">
-                                                <div class="testimonilas-text">
-                                                    <div class="testi-header">
-                                                        <div class="testi-avatar"><img src="images/avatar/3.jpg" alt=""></div>
-                                                        <h3>Centa Simpson</h3>
-                                                    </div>
-                                                    <div class="testimonilas-text-item">
-                                                        <div class="testimonilas-text-item-wrap">
-                                                            <p>"Vestibulum orci felis, ullamcorper non condimentum non, ultrices ac nunc. Mauris non ligula suscipit, vulputate mi accumsan, dapibus felis. Nullam sed sapien dui. Nulla auctor sit amet sem non porta. Lorem ipsum dolor sit amet, conse ctetuer adipiscing elit, sed diam nonu mmy nibh euismod . "</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="testi-footer">
-                                                    <a href="#" class="testi-link" target="_blank">Via X-Twitter</a>
-                                                    <span class="testi-number">01.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--testi-item end-->
-                                        <!--testi-item-->
-                                        <div class="swiper-slide">
-                                            <div class="testi-item">
-                                                <div class="testimonilas-text">
-                                                    <div class="testi-header">
-                                                        <div class="testi-avatar"><img src="images/avatar/2.jpg" alt=""></div>
-                                                        <h3>Andy Dimasky</h3>
-                                                    </div>
-                                                    <div class="testimonilas-text-item">
-                                                        <div class="testimonilas-text-item-wrap">
-                                                            <p>"Lorem ipsum dolor sit amet, conse ctetuer adipiscing elit, sed diam nonu mmy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Vestibulum orci felis, ullamcorper non condimentum non, ultrices ac nunc. Mauris non ligula suscipit sed diam nonu "</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="testi-footer">
-                                                    <a href="#" class="testi-link" target="_blank">Via Facebook</a>
-                                                    <span class="testi-number">02.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--testi-item end-->
-                                        <!--testi-item-->
-                                        <div class="swiper-slide">
-                                            <div class="testi-item">
-                                                <div class="testimonilas-text">
-                                                    <div class="testi-header">
-                                                        <div class="testi-avatar"><img src="images/avatar/1.jpg" alt=""></div>
-                                                        <h3>Jannet Dellov</h3>
-                                                    </div>
-                                                    <div class="testimonilas-text-item">
-                                                        <div class="testimonilas-text-item-wrap">
-                                                            <p>"Ultrices ac nunc. Mauris non ligula suscipit, vulputate mi accumsan, dapibus felis. Nullam sed sapien dui. Nulla auctor sit amet sem non porta. Lorem ipsum dolor sit amet, conse ctetuer adipiscing elit, sed diam nonu mmy nibh euismod sed diam nonu . "</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="testi-footer">
-                                                    <a href="#" class="testi-link" target="_blank">Via Instagram</a>
-                                                    <span class="testi-number">03.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--testi-item end-->
-                                        <!--testi-item-->
-                                        <div class="swiper-slide">
-                                            <div class="testi-item">
-                                                <div class="testimonilas-text">
-                                                    <div class="testi-header">
-                                                        <div class="testi-avatar"><img src="images/avatar/5.jpg" alt=""></div>
-                                                        <h3>Nicolo Svensky</h3>
-                                                    </div>
-                                                    <div class="testimonilas-text-item">
-                                                        <div class="testimonilas-text-item-wrap">
-                                                            <p>"Amet, conse ctetuer adipiscing elit, sed diam nonu mmy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Vestibulum orci felis, ullamcorper non condimentum non, ultrices ac nunc. Mauris non ligula suscipit, vulputate mi accumsan, dapibus felis. "</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="testi-footer">
-                                                    <a href="#" class="testi-link" target="_blank">Via X-Twitter</a>
-                                                    <span class="testi-number">04.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--testi-item end-->
-                                    </div>
-                                </div>
-                                <div class="tc-button tc-button-next"><i class="fas fa-caret-right"></i></div>
-                                <div class="tc-button tc-button-prev"><i class="fas fa-caret-left"></i></div>
-                            </div>
-                            <div class="fwc-controls_wrap">
-                                <div class="solid-pagination_btns tcs-pagination_init"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- section end  -->
+
+                    <!-- Testimonials  -->
+                    <?php include_once('includes/testimonials.php') ?>
+                    <!-- Testimonialsend  -->
+                     
                     <!--container-->
                     <div class="container">
                         <div class="api-wrap">
