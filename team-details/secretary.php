1b69d4011cab978bac9d5868c8fe4bac0230d354
Samuel Osei Adu <asamuel355@yahoo.com>
2024-12-09 22:44:33 +0000

    update on team details

---
+++ team-details/secretary.php
@@ -0,0 +1,291 @@
+<?php include('../includes/head.php') ?>
+
+<body>
+  <!--loader-->
+  <?php include_once('../includes/loader.php') ?>
+  <!--loader end-->
+  <!--  main   -->
+  <div id="main">
+    <?php include_once('../includes/header.php') ?>
+    <div class="body-overlay fs-wrapper search-form-overlay close-search-form"></div>
+    <!--header-end-->
+    <!--warpper-->
+    <div class="wrapper">
+      <!--content-->
+      <div class="content">
+        <!--section-->
+        <div class="section hero-section hero-section_sin">
+          <div class="hero-section-wrap">
+            <div class="hero-section-wrap-item">
+              <div class="container">
+                <div class="hero-section-container">
+                  <div class="hero-section-title">
+                    <h2>Secretary</h2>
+                  </div>
+                </div>
+              </div>
+              <div class="hs-scroll-down-wrap">
+                <div class="scroll-down-item">
+                  <div class="mousey">
+                    <div class="scroller"></div>
+                  </div>
+                  <span>Scroll Down To Discover</span>
+                </div>
+                <div class="svg-corner svg-corner_white" style="bottom:0;right: -39px; transform: rotate( 90deg)"></div>
+                <div class="svg-corner svg-corner_white" style="bottom:0;left:  -39px;"></div>
+              </div>
+              <div class="bg-wrap bg-hero bg-parallax-wrap-gradien fs-wrapper" data-scrollax-parent="true">
+                <div class="bg" data-bg="/images/bg/slider-1.jpg" data-scrollax="properties: { translateY: '30%' }"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--section-end-->
+        <!--container-->
+        <div class="container">
+          <!--breadcrumbs-list-->
+          <div class="breadcrumbs-list bl_flat">
+            <a href="/">Home</a>
+            <span>Secretary</span>
+            <div class="breadcrumbs-list_dec">
+              <i class="fa-thin fa-arrow-up"></i>
+            </div>
+          </div>
+          <!--breadcrumbs-list end-->
+          <!--main-content-->
+          <div class="main-content">
+            <!--boxed-container-->
+            <div class="boxed-container">
+              <div class="scroll-content-wrap">
+                <div class="row">
+                  <div class="col-lg-8">
+                    <div class="post-container">
+                      <div class="boxed-content">
+                        <div class="boxed-content-title">
+                        </div>
+                        <div class="boxed-content-item">
+                          <!-- team media -->
+                          <div class="blog-media">
+                            <div class="single-slider-wrap">
+                              <div class="single-slider">
+                                <div class="swiper-container">
+                                  <div class="swiper-wrapper lightgallery">
+                                    <div class="swiper-slide hov_zoom">
+                                      <img src="/images/team/ceo.jpg" alt="">
+                                      <a href="/images/team/ceo.jpg" class="box-media-zoom   popup-image">
+                                        <i class="fa fa-eye"></i>
+                                      </a>
+                                    </div>
+                                    <div class="swiper-slide hov_zoom">
+                                      <img src="/images/team/ceo2.jpg" alt="">
+                                      <a href="/images/team/ceo2.jpg" class="box-media-zoom   popup-image">
+                                        <i class="fal fa-eye"></i>
+                                      </a>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="fw-carousel-button-prev slider-button">
+                                <i class="fa-solid fa-caret-left"></i>
+                              </div>
+                              <div class="fw-carousel-button-next slider-button">
+                                <i class="fa-solid fa-caret-right"></i>
+                              </div>
+                              <div class="fwc-controls_wrap">
+                                <div class="solid-pagination_btns ss-slider-pagination"></div>
+                              </div>
+                            </div>
+                          </div>
+                          <!-- team media end -->
+                          <div class="text-block post-single_tb">
+                            <div class="text-block-container">
+                              <p>
+                                Mai S. Malakpa serves as the Secretary at Contours Limited, where she ensures the office operates efficiently through her organizational and administrative expertise. Her responsibilities include maintaining organized office tasks, implementing procedures to streamline workflows, and carrying out additional administrative duties to support the company's operations.
+                              </p>
+                              <p>
+                                Mai is instrumental in fostering an environment of professionalism and order, ensuring that all administrative functions are executed seamlessly. Her role is vital in supporting both the management and staff by handling day-to-day office operations with precision and efficiency.
+                              </p>
+                              <p>
+                                Mai’s dedication to maintaining high standards of office organization and her proactive approach to administrative responsibilities make her a key contributor to Contours Limited's success.
+                              </p>
+                            </div>
+                            <div class="tagcloud_single">
+                              <!--boxed-content-->
+                              <div class="boxed-content">
+                                <!--boxed-content-title-->
+                                <div class="boxed-content-title">
+                                  <h3>Connect with the Administrator</h3>
+                                </div>
+                                <!--boxed-content-title end-->
+                                <!--boxed-content-item-->
+                                <div class="boxed-content-item bc-item_smal_pad">
+                                  <div class="social-widget">
+                                    <a href="tel:231777788754">
+                                      <i class="fa-solid fa-phone"></i>
+                                    </a>
+                                    <a target="__blank" href="https://wa.me//231777788754">
+                                      <i class="fa-brands fa-whatsapp"></i>
+                                    </a>
+                                  </div>
+                                </div>
+                                <!--boxed-content-item end-->
+                              </div>
+                              <!--boxed-content end-->
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="content-nav-fixed dark-bg scale_anim">
+                        <ul>
+                          <li>
+                            <a href="/team-details/administrative-assistant" class="ln">
+                              <i class="fal fa-long-arrow-left"></i>
+                              <span>
+                                Prev <strong> - Admin As.</strong>
+                              </span>
+                            </a>
+                            <div class="content-nav_mediatooltip cnmd_leftside">
+                              <div class="overlay"></div>
+                              <div class="bg" data-bg="images/all/1.jpg"></div>
+                            </div>
+                          </li>
+                          <li>
+                            <a href="/team-details/surveyor" class="rn">
+                              <span>
+                                Next <strong> - Suveyor</strong>
+                              </span>
+                              <i class="fal fa-long-arrow-right"></i>
+                            </a>
+                            <div class="content-nav_mediatooltip cnmd_rightside">
+                              <div class="overlay"></div>
+                              <div class="bg" data-bg="images/all/3.jpg"></div>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <!--boxed-container-->
+                    <div class="sb-container fixed-bar">
+                      <!--boxed-content-->
+                      <div class="boxed-content">
+                        <!--boxed-content-title-->
+                        <div class="boxed-content-title">
+                          <h3>Other Staff Members</h3>
+                        </div>
+                        <!--boxed-content-title end-->
+                        <!--boxed-content-item-->
+                        <div class="boxed-content-item bc-item_smal_pad">
+                          <div class="recent-post-widget">
+                            <ul>
+                              <li>
+                                <div class="recent-post-img">
+                                  <a href="#">
+                                    <img src="/images/all/11.jpg" alt="">
+                                  </a>
+                                </div>
+                                <div class="recent-post-content">
+                                  <h4>
+                                    <a href="#">How to Book a Room online</a>
+                                  </h4>
+                                  <div class="recent-post-opt">
+                                    <span class="post-date">March 10, 2024</span>
+                                  </div>
+                                </div>
+                              </li>
+                              <li>
+                                <div class="recent-post-img">
+                                  <a href="#">
+                                    <img src="/images/all/11.jpg" alt="">
+                                  </a>
+                                </div>
+                                <div class="recent-post-content">
+                                  <h4>
+                                    <a href="#">How to Book a Room online</a>
+                                  </h4>
+                                  <div class="recent-post-opt">
+                                    <span class="post-date">March 10, 2024</span>
+                                  </div>
+                                </div>
+                              </li>
+                              <li>
+                                <div class="recent-post-img">
+                                  <a href="#">
+                                    <img src="/images/all/1.jpg" alt="">
+                                  </a>
+                                </div>
+                                <div class="recent-post-content">
+                                  <h4>
+                                    <a href="#">Snowy Mountains Trip</a>
+                                  </h4>
+                                  <div class="recent-post-opt">
+                                    <span class="post-date">August 10, 2023</span>
+                                  </div>
+                                </div>
+                              </li>
+                              <li>
+                                <div class="recent-post-img">
+                                  <a href="#">
+                                    <img src="/images/all/15.jpg" alt="">
+                                  </a>
+                                </div>
+                                <div class="recent-post-content">
+                                  <h4>
+                                    <a href="#">Traveller on our Hotel</a>
+                                  </h4>
+                                  <div class="recent-post-opt">
+                                    <span class="post-date">September 10, 2023</span>
+                                  </div>
+                                </div>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                        <!--boxed-content-item end-->
+                      </div>
+                      <!--boxed-content end-->
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="limit-box"></div>
+            </div>
+            <!--boxed-container end-->
+          </div>
+          <!--main-content end-->
+          <div class="to_top-btn-wrap">
+            <div class="to-top to-top_btn">
+              <span>Back to top</span>
+              <i class="fa-solid fa-arrow-up"></i>
+            </div>
+            <div class="svg-corner svg-corner_white" style="top:0;left:  -40px; transform: rotate(-90deg)"></div>
+            <div class="svg-corner svg-corner_white" style="top:0;right: -40px; transform: rotate(-180deg)"></div>
+          </div>
+        </div>
+        <!-- container end-->
+      </div>
+      <!--content  end-->
+      <!--main-footer-->
+      <div class="height-emulator"></div>
+      <?php include_once('../includes/footer.php') ?>
+      <!--main-footer end-->
+    </div>
+    <!--warpper end-->
+
+    <div class="mob-nav-overlay fs-wrapper"></div>
+    <div class="body-overlay fs-wrapper wishlist-wrap-overlay clwl_btn"></div>
+
+    <!-- progress-bar  -->
+    <div class="progress-bar-wrap">
+      <div class="progress-bar color-bg"></div>
+    </div>
+    <!-- progress-bar end -->
+  </div>
+  <!-- Main end -->
+
+  <!--=============== scripts  ===============-->
+  <?php include_once('../includes/script.php') ?>
+</body>
+
+</html>
